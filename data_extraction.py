b6f24a7420e12e9ccedc546a988811e6d5eed424
CSoetaert <camille.soetaert@gmail.com>
2018-01-07 12:42:59 +0100

    First attempt of deep neural network and modification to the data extraction methods

--- data_extraction.py
+++ data_extraction.py
@@ -1,4 +1,5 @@
 import csv
+import numpy as np
 
 def read_csv_get_values(file_name) : 
 	"""Function to read the csv files containing the data and put it
@@ -19,11 +20,22 @@ def read_csv_get_values(file_name) :
 		for row in csv_content:
 			for i in range(1,len(row)) : 
 				row[i] = float(row[i])
-			list_row.append(row)
+			list_row.append(row[1:])
 	return list_row
 
-data_sub1_ser1 = read_csv_get_values("Data/train/subj1_series1_data.csv")
-print data_sub1_ser1[0]
+def get_class_from_data(array_row):
+	labels = np.zeros(array_row.shape[0],dtype = int)
+	for i in range(array_row.shape[0]):
+		labels[i] = 6
+		for j in range(array_row.shape[1]):
+			if array_row[i,j] == 1.0:
+				labels[i] = int(j)
+	return labels
 
-events_sub1_ser1 = read_csv_get_values("Data/train/subj1_series1_events.csv")
-print events_sub1_ser1[0]
+
+if __name__ == "__main__":
+	data_sub1_ser1 = read_csv_get_values("Data/train/subj1_series1_data.csv")
+	print data_sub1_ser1[0]
+
+	events_sub1_ser1 = read_csv_get_values("Data/train/subj1_series1_events.csv")
+	print events_sub1_ser1[0]
---
+++ main.py
@@ -0,0 +1,55 @@
+import tensorflow as tf
+import data_extraction as dt
+import numpy as np
+
+FILE_TRAIN_SUB1_SER1_DATA = "Data/train/subj1_series1_data.csv"
+FILE_TRAIN_SUB1_SER1_EVENTS = "Data/train/subj1_series1_events.csv"
+
+FILE_TEST_SUB1_SER2_DATA = "Data/train/subj1_series2_data.csv"
+FILE_TEST_SUB1_SER2_EVENTS = "Data/train/subj1_series2_events.csv"
+
+def main(): 
+	session = tf.InteractiveSession()
+	features = [tf.feature_column.numeric_column("x",shape=[32])]
+	classifier = tf.estimator.DNNClassifier(
+		feature_columns = features,
+		hidden_units = [40,20,30,40,30,20,30,20,35,25,40],
+		n_classes = 7,
+		model_dir = "Model")
+
+	data_sub1_ser1 = np.array(dt.read_csv_get_values(FILE_TRAIN_SUB1_SER1_DATA))
+	events_sub1_ser1 = dt.get_class_from_data(np.array(dt.read_csv_get_values(FILE_TRAIN_SUB1_SER1_EVENTS)))
+
+	train_input_fn = tf.estimator.inputs.numpy_input_fn(
+		x={"x":data_sub1_ser1},
+		y=events_sub1_ser1,
+		num_epochs=None,
+		shuffle=True)
+	classifier.train(input_fn=train_input_fn,steps=4000)
+
+	data_sub1_ser9 = np.array(dt.read_csv_get_values(FILE_TEST_SUB1_SER2_DATA))
+	events_sub1_ser9 = np.array(dt.read_csv_get_values(FILE_TEST_SUB1_SER2_EVENTS))
+	labels_sub1_ser9 = dt.get_class_from_data(events_sub1_ser9)
+	test_input_fn = tf.estimator.inputs.numpy_input_fn(
+		x={"x": data_sub1_ser9},
+	    y=labels_sub1_ser9,
+	    num_epochs=1,
+	    shuffle=False)
+
+	evaluation = classifier.evaluate(input_fn=test_input_fn)
+	print evaluation
+
+	predict_input_fn = tf.estimator.inputs.numpy_input_fn(
+	    x={"x": data_sub1_ser9},
+	    num_epochs=1,
+	    shuffle=False)
+
+	predictions = classifier.predict(input_fn=predict_input_fn)
+	predicted_classes = [int(p["classes"][0]) for p in predictions]
+ 	#print predicted_classes
+ 	confusion_matrix = tf.contrib.metrics.confusion_matrix(labels_sub1_ser9,predicted_classes,num_classes=7)
+ 	session.run(confusion_matrix)
+ 	print confusion_matrix.eval()
+
+if __name__ == "__main__":
+    main()
